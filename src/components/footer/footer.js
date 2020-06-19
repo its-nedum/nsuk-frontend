120d96b892faf67d3239bbeca7aa7080abf00466
its-nedum <de.magnificent1@gmail.com>
2020-06-19 12:59:36 +0100

    feature(fixed footer)

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,10 +4,10 @@ import '../styles/footer.css'
 const Footer = () => {
     return (
         <div className="footer">
-            <div className="card">
-            <div className="card-footer text-muted text-center myfooter">
-                <p>&copy; 2020 E-Plus Networks, All rights reserved </p>
-            </div>
+            <div className="card card-bg">
+                <div className="card-footer text-muted text-center myfooter">
+                    <p>&copy; 2020 E-plus Networks, All rights reserved </p>
+                </div>
             </div>
             
         </div>
