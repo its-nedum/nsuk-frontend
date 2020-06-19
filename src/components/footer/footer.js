c01d42e454ef68b451e1cd48432bc981f5464691
its-nedum <de.magnificent1@gmail.com>
2020-06-19 12:49:35 +0100

    features(setting up the navbar)

---
+++ src/components/footer/footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styles/footer.css'
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="card">
+            <div className="card-footer text-muted text-center myfooter">
+                <p>&copy; 2020 E-Plus Networks, All rights reserved </p>
+            </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/pages/home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../header/navbar'
+import '../styles/home.css'
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            <div className="banner_container">
+                <div className="banner">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
