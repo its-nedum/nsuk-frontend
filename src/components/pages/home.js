cd151fd6087880925d71598448b84e11f7fae299
its-nedum <de.magnificent1@gmail.com>
2020-06-20 19:31:47 +0100

    features(completed login form)

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../header/navbar'
 import '../styles/home.css'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
@@ -14,6 +15,28 @@ const Home = () => {
 
                             </div>
                             <div className="col-sm-12 col-md-4">
+                                <form className="login_form">
+                                    <h3>Login</h3>
+                                <div className="input-group mb-3">   
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text" id="basic-addon1"><i class="fas fa-envelope"></i></span>
+                                    </div>
+                                <input type="email" className="form-control" placeholder="Email" aria-label="email" aria-describedby="basic-addon1" />
+                                </div>
+
+                                <div className="input-group mb-3">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text" id="basic-addon1"><i className="fa fa-key" aria-hidden="true"></i></span>
+                                    </div>
+                                <input type="password" className="form-control" placeholder="Password" aria-label="password" aria-describedby="basic-addon1" />
+                                </div>
+                                <input type="checkbox" /> Remember me
+                                <p><Link to="!#">Forgot password?</Link></p>
+                                <div className="input-group mb-3">
+                                    <input type="submit" className="form-control btn btn-primary" value="Login" />
+                                </div>
+                                <p>Don't have an account? <Link to="!#">Register</Link></p>
+                                </form>
 
                             </div>
                         </div>
