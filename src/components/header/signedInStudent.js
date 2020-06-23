bc27da646778c70f90b0e56c31c9a0a1bc2d80aa
its-nedum <de.magnificent1@gmail.com>
2020-06-23 16:41:54 +0100

    bug(fixed error with contact page)

---
+++ src/components/header/signedInStudent.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import '../styles/navbar.css'
+import logo from '../../images/logo.png'
+
+const Navbar = () => {
+    return (
+        <div className="container-fluid nav_holder">
+            <nav className="navbar navbar-expand-lg navbar-light">
+                <Link className="navbar-brand" to="/"><img src={logo} alt="logo" className="logo responsive-img" /></Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto">
+                    <li className="nav-item active">
+                        <Link className="nav-link" to="/">POST UTME <span className="sr-only">(current)</span></Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/instructions">INSTRUCTION</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/contact-us">CONTACT US</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/signout">SIGN OUT</Link>
+                    </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
