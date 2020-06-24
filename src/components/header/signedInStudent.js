6477dcfa560fbc81de52309067ec2c7965ddff1c
its-nedum <de.magnificent1@gmail.com>
2020-06-24 10:58:46 +0100

    chores(trying some fix)

--- src/components/header/signedInStudent.js
+++ src/components/header/signedInStudent.js
@@ -15,7 +15,7 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ml-auto">
                     <li className="nav-item active">
-                        <Link className="nav-link" to="/">POST UTME <span className="sr-only">(current)</span></Link>
+                        <Link className="nav-link" to="/dashboard">DASHBOARD<span className="sr-only">(current)</span></Link>
                     </li>
                     <li className="nav-item">
                         <Link className="nav-link" to="/instructions">INSTRUCTION</Link>
--- src/components/pages/students/dashboard.js
+++ src/components/pages/students/dashboard.js
@@ -1,11 +1,21 @@
 import React from 'react'
-import Navbar from '../../header/navbar'
+import Navbar from '../../header/signedInStudent'
+import '../../styles/dashboard.css'
 
 const Dashboard = () => {
     return (
         <div>
             <Navbar />
-            
+            <div className="dashboard_banner">
+                <div className="row">
+                    <div className="col-sm-12 col-md-2 dash_menu">
+
+                    </div>
+                    <div className="col-sm-12 col-md-10 dash_option">
+                    
+                    </div>
+                </div>
+            </div>    
         </div>
     )
 }
