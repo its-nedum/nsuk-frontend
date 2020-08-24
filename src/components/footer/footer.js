0527bb3b14c6d0c1672b631b081fe9f7d9d3f209
its-nedum <de.magnificent1@gmail.com>
2020-08-24 12:19:56 +0100

    chores(changed footer note)

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
         <div className="footer">
             <div className="card card-bg">
                 <div className="card-footer text-muted text-center myfooter">
-                    <p>&copy; 2020 NSUK Admissions | Powered by E-plus Networks | All rights reserved. </p>
+                    <p>&copy; 2020 NSUK Admissions. All rights reserved | Developed with <span className="text-danger">♥</span> by <a href="https://chineduemesue.netlify.app" target="_blank" rel="noopener noreferrer">Chinedu Emesue</a></p>
                 </div>
             </div>
             
